af7ff7bcd06440858fa9e581eac51d371deb1a0a
Kaan C. Fidan <kaancfidan@gmail.com>
2020-05-26 02:23:50 +0300

    Refactor/main (#18)

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/http/httputil"
@@ -12,69 +13,95 @@ import (
 	"github.com/kaancfidan/bouncer/services"
 )
 
-var (
-	version       = "0.0.0-VERSION" // to be replaced in CI
+const version = "0.0.0-VERSION" // to be replaced in CI
+
+type flags struct {
 	hmacKey       string
-	configPath    = "/var/lib/bouncer/config.yaml"
+	configPath    string
 	upstreamURL   string
-	listenAddress = ":3512"
-)
+	listenAddress string
+}
 
 func main() {
-	flag.StringVar(&hmacKey, "k",
-		lookupEnv("BOUNCER_SIGNING_KEY", ""),
-		"symmetric signing key to validate tokens")
+	f := parseFlags()
 
-	flag.StringVar(&configPath, "p",
-		lookupEnv("BOUNCER_CONFIG_PATH", configPath),
-		fmt.Sprintf("Config YAML path, default = %s", configPath))
-
-	flag.StringVar(&upstreamURL, "u",
-		lookupEnv("BOUNCER_UPSTREAM_URL", ""),
-		"URL to be called when the request is authorized")
-
-	flag.StringVar(&listenAddress, "l",
-		lookupEnv("BOUNCER_LISTEN_ADDRESS", listenAddress),
-		fmt.Sprintf("listen address, default = %s", listenAddress))
-
-	flag.Parse()
-
-	// parse upstream URL
-	parsedURL, err := url.Parse(upstreamURL)
-	if err != nil {
-		log.Fatalf("upstream url could not be parsed: %v", err)
-	}
-
-	configReader, err := os.Open(configPath)
+	configReader, err := os.Open(f.configPath)
 	if err != nil {
 		log.Fatalf("could not open config file: %v", err)
+	}
+	defer configReader.Close()
+
+	server, err := newServer(f, configReader)
+	if err != nil {
+		log.Fatalf("server could not be created: %v", err)
+	}
+
+	http.HandleFunc("/", server.Proxy)
+
+	log.Printf("Bouncer[%s] started.", version)
+	defer log.Printf("Bouncer shut down.")
+
+	err = http.ListenAndServe(f.listenAddress, nil)
+	log.Fatal(err)
+}
+
+func newServer(f *flags, configReader io.Reader) (*services.Server, error) {
+	// parse upstream URL
+	parsedURL, err := url.Parse(f.upstreamURL)
+	if err != nil {
+		return nil, fmt.Errorf("upstream url could not be parsed: %w", err)
+	}
+
+	if parsedURL.Scheme != "http" && parsedURL.Scheme != "https" {
+		return nil, fmt.Errorf("upstream url scheme must be http or https")
 	}
 
 	parser := services.YamlConfigParser{}
 	cfg, err := parser.ParseConfig(configReader)
 	if err != nil {
-		log.Fatalf("could not parse config: %v", err)
+		return nil, fmt.Errorf("could not parse config: %w", err)
 	}
 
 	err = services.ValidateConfig(cfg)
 	if err != nil {
-		log.Fatalf("invalid config: %v", err)
+		return nil, fmt.Errorf("invalid config: %w", err)
 	}
 
-	server := services.Server{
+	s := services.Server{
 		Upstream:      httputil.NewSingleHostReverseProxy(parsedURL),
 		RouteMatcher:  services.NewRouteMatcher(cfg.RoutePolicies),
 		Authorizer:    services.NewAuthorizer(cfg.ClaimPolicies),
-		Authenticator: services.NewAuthenticator([]byte(hmacKey)),
+		Authenticator: services.NewAuthenticator([]byte(f.hmacKey)),
 	}
 
-	http.HandleFunc("/", server.Proxy)
+	return &s, nil
+}
 
-	log.Printf("Bouncer [%s] started.", version)
-	defer log.Printf("Bouncer shut down.")
+func parseFlags() *flags {
+	p := flags{
+		configPath:    "/etc/bouncer/config.yaml",
+		listenAddress: ":3512",
+	}
 
-	err = http.ListenAndServe(listenAddress, nil)
-	log.Fatal(err)
+	flag.StringVar(&p.hmacKey, "k",
+		lookupEnv("BOUNCER_SIGNING_KEY", ""),
+		"symmetric signing key to validate tokens")
+
+	flag.StringVar(&p.configPath, "p",
+		lookupEnv("BOUNCER_CONFIG_PATH", p.configPath),
+		fmt.Sprintf("Config YAML path, default = %s", p.configPath))
+
+	flag.StringVar(&p.upstreamURL, "u",
+		lookupEnv("BOUNCER_UPSTREAM_URL", ""),
+		"URL to be called when the request is authorized")
+
+	flag.StringVar(&p.listenAddress, "l",
+		lookupEnv("BOUNCER_LISTEN_ADDRESS", p.listenAddress),
+		fmt.Sprintf("listen address, default = %s", p.listenAddress))
+
+	flag.Parse()
+
+	return &p
 }
 
 func lookupEnv(key string, defaultVal string) string {
---
+++ main_test.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func Test_newServer(t *testing.T) {
+	tests := []struct {
+		name       string
+		flags      *flags
+		cfgContent string
+		wantErr    bool
+	}{
+		{
+			name: "happy path",
+			flags: &flags{
+				hmacKey:     "SuperSecretKey123!",
+				upstreamURL: "http://localhost:8080",
+			},
+			cfgContent: "claimPolicies: {}\nroutePolicies: []",
+			wantErr:    false,
+		},
+		{
+			name: "no config",
+			flags: &flags{
+				hmacKey:     "SuperSecretKey123!",
+				upstreamURL: "http://localhost:8080",
+			},
+			cfgContent: "",
+			wantErr:    true,
+		},
+		{
+			name: "invalid url scheme",
+			flags: &flags{
+				hmacKey:     "SuperSecretKey123!",
+				upstreamURL: "tcp://localhost:8080",
+			},
+			cfgContent: "claimPolicies: {}\nroutePolicies: []",
+			wantErr:    true,
+		},
+		{
+			name: "malformed url",
+			flags: &flags{
+				hmacKey:     "SuperSecretKey123!",
+				upstreamURL: "!!http://localhost:8080",
+			},
+			cfgContent: "claimPolicies: {}\nroutePolicies: []",
+			wantErr:    true,
+		},
+		{
+			name: "no signing key still creates server",
+			flags: &flags{
+				upstreamURL: "http://localhost:8080",
+			},
+			cfgContent: "claimPolicies: {}\nroutePolicies: []",
+			wantErr:    false,
+		},
+		{
+			name: "invalid config yaml",
+			flags: &flags{
+				hmacKey:     "SuperSecretKey123!",
+				upstreamURL: "http://localhost:8080",
+			},
+			cfgContent: ": invalid",
+			wantErr:    true,
+		},
+		{
+			name: "invalid config content",
+			flags: &flags{
+				hmacKey:     "SuperSecretKey123!",
+				upstreamURL: "http://localhost:8080",
+			},
+			cfgContent: "claimPolicies:\n PolicyWithoutClaim:\n  - value: test\nroutePolicies: []",
+			wantErr:    true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			buf := bytes.Buffer{}
+			buf.WriteString(tt.cfgContent)
+
+			_, err := newServer(tt.flags, &buf)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("newServer() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
